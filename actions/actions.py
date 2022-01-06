bb5ab22adb7deda4d4062ef9d1528a4d72efcbce
Aayush1011 <aayush1000adhikari@gmail.com>
2022-01-06 11:05:25 +0545

    GO

--- actions/actions.py
+++ actions/actions.py
@@ -7,12 +7,13 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet, EventType
+
+
 # class ActionHelloWorld(Action):
 #
 #     def name(self) -> Text:
@@ -25,3 +26,35 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+
+class ValidateLaptopForm(Action):
+    def name(self) -> Text:
+        return "laptop_form"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict) -> List[EventType]:
+        required_slots = ["usecase", "price", "brand"]
+
+        for slot_name in required_slots:
+            if tracker.slots.get(slot_name) is None:
+                # The slot is not filled yet. Request the user to fill this slot next.
+                return [SlotSet("requested_slot", slot_name)]
+
+        # All slots are filled.
+        return [SlotSet("requested_slot", None)]
+
+class ActionSubmit(Action):
+    def name(self) -> Text:
+        return "action_submit"
+
+    def run(
+        self,
+        dispatcher,
+        tracker: Tracker,
+        domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_slot_values",
+                                 Usecase=tracker.get_slot("usecase"),
+                                 Price=tracker.get_slot("price"),
+                                 Brand=tracker.get_slot("brand"))
